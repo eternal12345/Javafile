844677dabec24a5553e3fd4af77b5f5e71b013c5
NickAdams <34204787+eternal12345@users.noreply.github.com>
2019-11-15 21:01:11 +0800

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,16 @@
+//Student.java
+package ceshi;
+
+public class Student {
+	public String name;
+	public int StuId;
+	
+	public Student(String name, int Stuid) {
+		this.name = name;
+		this.StuId = Stuid;
+		
+		
+	}
+	
+
+}
